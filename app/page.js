4493a6174e00e30462f2363bcf18e06a14be430d
hasuhasmath <hasuh5pepe@gmail.com>
2025-09-04 14:39:28 +0530

    feat (services) : Implemented Services Page

--- app/page.js
+++ app/page.js
@@ -10,27 +10,27 @@ const Home = () => {
   return (
     <section className="full">
       <div className="container mx-auto ">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-0 xl:pb-2">
           <div className="text-center xl:text-left order-2 xl:order-none">
-            <span className="text-xl">Software Engineer</span>
+            <span className="text-xl mb-2">Software Engineer</span>
             <h1 className="h1">
               Hello I am <br />
               <span className="text-accent">Mohamed Hasmath</span>
             </h1>
-            <p className="max-w-[500px] mb-9 text-white/80">
+            <p className="max-w-[500px] mt-2 mb-9 text-white/80">
               I am excel at crafting elegant digital experience and I am
               proficient in various programming language and technologies
             </p>
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
                 variant="outline"
-                size="lg"
-                className="uppercase flex items-center gap-1"
+                size="md"
+                className="uppercase flex items-center gap-2"
               >
                 <span>Download CV</span>
                 <FiDownload className="text-l" />
               </Button>
-              <div className="mb-8 xl:mb-8">
+              <div className="mb-8 xl:mb-3">
                 <Socials
                   containerStyles="flex gap-6"
                   iconStyles="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,7 +1,90 @@
-import React from "react";
+"use client";
+
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+
+const services = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Building responsive and dynamic websites using modern technologies.",
+    href: "#",
+  },
+  {
+    num: "02",
+    title: "UI/UX Design",
+    description:
+      " Crafting user-friendly interfaces and engaging user experiences.",
+    href: "#",
+  },
+  {
+    num: "03",
+    title: "Machine Learning/Deep Learning",
+    description:
+      "Implementing intelligent systems and predictive models using ML/DL techniques.",
+    href: "#",
+  },
+  {
+    num: "04",
+    title: "SEO",
+    description:
+      "Optimizing websites to improve search engine rankings and visibility.",
+    href: "#",
+  },
+];
+
+import { motion } from "framer-motion";
 
 const Services = () => {
-  return <div>Services --</div>;
+  return (
+    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+          }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {services.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="flex flex-col justify-center gap-6 group"
+              >
+                <div className="w-full flex justify-between items-center">
+                  <div
+                    className="text-4xl font-extrabold text-outline text-transparent 
+                  group-hover:text-outline-hover transition-all duration-500"
+                  >
+                    {item.num}
+                  </div>
+                  <Link
+                    href={item.href}
+                    className="w-[50px] h-[50px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45"
+                  >
+                    <BsArrowDownRight className="text-primary text-3xl" />
+                  </Link>
+                </div>
+                {/* title */}
+                <h2 className="text-[22px] font-bold leading-none text-white  group-hover:text-accent transition-all duration-500">
+                  {item.title}
+                </h2>
+                {/* description */}
+                <h2 className="text-white/60 group-hover:text-accent">
+                  {item.description}
+                </h2>
+                {/* border */}
+                <div className="border-b border-white/20  w-full "></div>
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
